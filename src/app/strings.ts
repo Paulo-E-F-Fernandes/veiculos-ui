89132c4255b391f036384b8b84790b6231fbc667
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2018-05-14 23:47:35 -0300

    Criação de uma classe para centralizar as mensagens

---
+++ src/app/strings.ts
@@ -0,0 +1,31 @@
+export class Strings {
+
+    // Buttons
+    btnAdicionar: String = 'Adicionar';
+    btnAtualizar: String = 'Atualizar';
+    btnCancelar: String = 'Cancelar';
+    btnFechar: String = 'Fechar';
+
+    // Labels
+    lblAcoes: String = 'Ações';
+    lblAno: String = 'Ano';
+    lblAnoFabricacao: String = 'Ano de Fabricação';
+    lblAnoModelo: String = 'Ano do Modelo';
+    lblFabricacao: String = 'Fabricação';
+    lblCor: String = 'Cor';
+    lblModelo: String = 'Modelo';
+    lblMontadora: String = 'Montadora';
+    lblPlaca: String = 'Placa';
+    lblUltimaAlteracao: String = 'Última Alteração';
+    lblValor: String = 'Valor';
+    lblValorMercado: String = 'Valor de mercado';
+    lblVeiculoDetalhe: String = 'Detalhes do Veículo';
+    
+    // Mensagens
+    msgCadastroVeiculo: String = 'Cadastro de Veículos';
+    msgSemRegistros: String = 'Nenhum resgistro encontrado!';
+
+
+          
+
+}
